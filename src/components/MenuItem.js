657599e3a8a51ffad330862b4f28a2258e1c3347
HaSandhu <sandhunhs@gmail.com>
2024-02-09 00:01:01 -0600

    MenuItem is same from p0 now

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -4,15 +4,15 @@ const MenuItem = ({ title, description, imageName, price }) => {
     const imageUrl = process.env.PUBLIC_URL + `/images/${imageName}`;
 
     return (
-        <div className="d-flex menu-item my-3">
-            <div className="flex-shrink-0">
-                <img src={imageUrl} className="menu-item-image" alt={title} />
+        <div className="row menu-item">
+            <div className="col-4">
+                <img src={imageUrl} alt={title} className="menu-item-image" />
             </div>
-            <div className="flex-grow-1 ms-3">
-                <h5 className="mt-0">{title}</h5>
-                <p>{description}</p>
+            <div className="col-8">
+                <h3 className="item-title">{title}</h3>
+                <p className="item-description">{description}</p>
                 <div className="d-flex justify-content-between align-items-center">
-                    <span className="price">${price}</span>
+                    <p className="price me-2">${price}</p>
                     <button className="btn add-button">Add</button>
                 </div>
             </div>
